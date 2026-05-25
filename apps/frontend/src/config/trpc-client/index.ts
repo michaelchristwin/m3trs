0b1ee74b644f3a0402d53896638cea42c578eb05
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-05-25 09:06:32 +0100

    Modified backend URL

--- apps/frontend/src/config/trpc-client/index.ts
+++ apps/frontend/src/config/trpc-client/index.ts
@@ -1,10 +1,12 @@
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "@m3trs/trpc/router";
 
+const server = import.meta.env.VITE_BACKEND_URL;
+
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/trpc",
+      url: `${server}/trpc`,
     }),
   ],
 });
