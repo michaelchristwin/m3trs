121153f8446690ac27e2cb1c563718ff903b4c7b
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-06-15 18:45:23 +0100

    chore: Whitelisted new domains

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,7 +4,16 @@ import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "@m3trs/trpc/router";
 
 const app = new Hono();
-app.use(cors({ origin: ["http://localhost:5173", "https://m3trs.pages.dev"] }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://m3trs.pages.dev",
+      "https://m3trs.m3ter.ing",
+      "https://trs.m3ter.ing",
+    ],
+  }),
+);
 
 app.use("/trpc/*", trpcServer({ router: appRouter }));
 
